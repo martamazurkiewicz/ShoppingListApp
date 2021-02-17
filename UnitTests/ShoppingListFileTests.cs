33d5db126af32355d06834fba7b62f7561382ea8
martamazurkiewicz <marta@mazurkiewi.cz>
2021-02-17 19:54:56 +0100

    Added test for creating shopping list

--- UnitTests/ShoppingListFileTests.cs
+++ UnitTests/ShoppingListFileTests.cs
@@ -1,6 +1,50 @@
-﻿namespace UnitTests
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using ShoppingListApp.Entities;
+namespace UnitTests
 {
-    public class ShoppingListFileTests
+    public class CreatingFileTests
     {
+        [TestCase]
+        public void CreateNewFile()
+        {
+            var shoppingListFile = new ShoppingListFile();
+            List<Product> products = shoppingListFile.GetProducts();
+            Assert.AreEqual(new List<Product>(), products);
+        }
+
+        [TestCase(@"C:\Repo\ShoppingListApp\UnitTests\bin\shoppingList.txt")]
+        public void LoadFile(string path)
+        {
+            var products = new List<Product>
+            {
+                new Product("AAA batteries", new Category("Ebay")),
+                new Product("spinach", new Category("market")),
+                new Product("coffee", new Category("super market"))
+            };
+            CreateJsonFile(path, products);
+            var shoppingListFile = new ShoppingListFile(path);
+            Assert.AreEqual(products, shoppingListFile.GetProducts());
+        }
+
+        [TestCase]
+        public void CreateNewFileWhenFileAlreadyExists()
+        {
+            CreateJsonFile("shoppingList.txt", new List<Product>());
+            Assert.Throws<FileAreadyExistsException>(() => new ShoppingListFile());
+        }
+        
+        private void CreateJsonFile(string path, List<Product> products)
+        {
+            using (StreamWriter file = File.CreateText(path))
+            {
+                var serializer = new JsonSerializer();
+                serializer.Serialize(file, products);
+            }
+        }
     }
+    
 }
